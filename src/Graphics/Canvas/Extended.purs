9e0ae565518f5ff9528353acfb8687b96c110b6f
jutaro <jnf@arcor.de>
2015-04-03 13:53:33 +0200

    Add Image for Graphics/Canvas, which is missing in Canvas.

---
+++ src/Graphics/Canvas/Extended.purs
@@ -0,0 +1,17 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Graphics.Canvas.Extended
+-- Copyright   :  Jürgen Nicklisch-Franken
+-- License     :  Apache-2.0
+--
+-- Maintainer  :  jnf@arcor.de
+-- Stability   :
+-- Portability :
+--
+-- | What I miss in Canvas binding
+--
+-----------------------------------------------------------------------------
+
+module Graphics.Canvas.Extended where
+
+foreign import data Image :: *
