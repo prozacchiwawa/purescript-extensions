0782eeb6154c565b5c9bdda52aa2ffffdd74fce5
jutaro <jnf@arcor.de>
2015-07-19 20:48:29 +0200

    Add minInt.

--- src/Extensions.purs
+++ src/Extensions.purs
@@ -34,5 +34,10 @@ mapM f array = sequence (map f array)
 -- | Map with effects over an array of values.
 foreign import mapE :: forall a b e. (a -> Eff e b) -> Array a -> Eff e (Array b)
 
+-- Should go to Data.Int
+minInt :: Int -> Int -> Int
+minInt a b | a < b = a
+           | otherwise = b
+
 -- Should go to: Graphics.Canvas
 foreign import data Image :: *
