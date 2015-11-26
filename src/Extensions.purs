3746a4f9e8fa99c551cdf5450fb5a632d6aa51f9
jutaro <jnf@arcor.de>
2015-11-26 13:22:35 +0100

    Add hRange, remove minInt.

--- src/Extensions.purs
+++ src/Extensions.purs
@@ -14,9 +14,29 @@ module Extensions where
 import Prelude
 import Data.Traversable(sequence)
 import Control.Monad.Eff
+import Data.Array (range)
+import Math(floor)
 
 (>>) :: forall m a b. (Bind m) => m a -> m b -> m b
 (>>) x y = x >>= const y
+
+-- A range function, which only counts up like in Haskell
+hRange :: Int -> Int -> Array Int
+hRange a b = if a > b then [] else range a b
+
+-- A real mod function for Ints (not a remainder like % and mod in purescript)
+modInt :: Int -> Int -> Int
+modInt n d    =  if signum r == negate (signum d) then r+d else r
+                    where r = n `mod` d
+
+signum :: Int -> Int
+signum n | n > 0 = 1
+signum n | n < 0 = -1
+signum n | otherwise = 0
+
+-- | A mod function for Floats
+modFloat :: Number -> Number -> Number
+modFloat n d = n - (floor (n / d)) * d
 
 foreign import data TIMEOUT :: !
 
@@ -40,11 +60,6 @@ mapM f array = sequence (map f array)
 
 -- | Map with effects over an array of values.
 foreign import mapE :: forall a b e. (a -> Eff e b) -> Array a -> Eff e (Array b)
-
--- Should go to Data.Int
-minInt :: Int -> Int -> Int
-minInt a b | a < b = a
-           | otherwise = b
 
 -- Should go to: Graphics.Canvas
 foreign import data Image :: *
